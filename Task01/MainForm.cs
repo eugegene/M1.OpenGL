373ceaa8f24bd2757ed8dcf6e533652014541b3b
Yevhenii Petrenko <evgen151218@gmail.com>
2024-10-15 15:22:33 +0300

    Task01

---
+++ Task01/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+
+namespace PR1_Petrenko_program
+{
+    public partial class MainForm : Form
+    {
+        //private RenderControl renderControl;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
