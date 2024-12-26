254a97b043a4be19d03fb7d01b954f1dd5f9a832
Yevhenii Petrenko <evgen151218@gmail.com>
2024-12-26 14:17:04 +0200

    Task06_2

---
+++ Task06/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace PR6_Petrenko_program
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            renderControl1 = new RenderControl();
+            lblInfo = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // renderControl1
+            // 
+            renderControl1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            renderControl1.BackColor = System.Drawing.Color.SlateGray;
+            renderControl1.ForeColor = System.Drawing.Color.White;
+            renderControl1.Location = new System.Drawing.Point(12, 12);
+            renderControl1.MinimumSize = new System.Drawing.Size(550, 400);
+            renderControl1.Name = "renderControl1";
+            renderControl1.S = 0.2D;
+            renderControl1.Size = new System.Drawing.Size(655, 408);
+            renderControl1.TabIndex = 0;
+            renderControl1.TextCodePage = 65001;
+            // 
+            // lblInfo
+            // 
+            lblInfo.AutoSize = true;
+            lblInfo.Location = new System.Drawing.Point(431, 13);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new System.Drawing.Size(235, 60);
+            lblInfo.TabIndex = 1;
+            lblInfo.Text = "Use Mouse to control camera\r\n\r\nUse Arrow Keys to control kinematic model\r\nUp/Down, Left/Right, PageUp/PageDown";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(679, 431);
+            Controls.Add(lblInfo);
+            Controls.Add(renderControl1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Main Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RenderControl renderControl1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
